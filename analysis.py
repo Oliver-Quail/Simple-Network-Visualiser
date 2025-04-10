89d693a3a3cd0d6d6a1d7a6e076f9726a17ada1f
oliver <oliver.quail3@gmail.com>
2025-04-10 20:22:08 +1000

    Added way to get source and destination ips

--- analysis.py
+++ analysis.py
@@ -1,13 +1,28 @@
 import pyshark
+import sqlite3
 
-file_location = input("Please enter the location of the pcap file: ")
-
+#file_location = input("Please enter the location of the pcap file: ")
+file_location = "./samples/malware.pcap"
 
 try:
-    data = pyshark.FileCapture(file_location)
-
+    capture = pyshark.FileCapture(file_location)
+    print("Loaded capture")
 except FileNotFoundError:
     print("The file name you entered is invalid!")
     print("Exiting...")
     exit()
+
+
+for packet in capture:
+
+    try:
+        # fetch the source IP
+        source_ip = packet.ip.src
+        destination_ip = packet.ip.dst
+        print(source_ip)
+    except:
+        print("failed")
+        continue
+    
+    pass
 
