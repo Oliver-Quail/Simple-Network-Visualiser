34172de1d4c04e6ba08a8506ff29f4845edea15e
oliver <oliver.quail3@gmail.com>
2025-04-15 17:52:24 +1000

    Added load button to graphs

--- web_interface/src/pages/analysis/analysis.tsx
+++ web_interface/src/pages/analysis/analysis.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { applyNodeChanges, Edge, Node, NodeChange, OnEdgesChange, OnNodesChange, ReactFlow, ReactFlowProvider, useEdgesState, useNodesState, useReactFlow, XYPosition } from '@xyflow/react';
+import { applyNodeChanges, Edge, MiniMap, Node, NodeChange, OnEdgesChange, OnNodesChange, ReactFlow, ReactFlowProvider, useEdgesState, useNodesState, useReactFlow, XYPosition } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import CustomEdge from '../../components/customEdge';
 import { useCallback, useEffect, useState } from 'react';
 import ELK, { ElkExtendedEdge, ElkNode, LayoutOptions } from 'elkjs/lib/elk.bundled.js';
+import { wait } from '@testing-library/user-event/dist/utils';
 
 const edgeTypes = {"custom-edge" : CustomEdge}
 
@@ -28,16 +29,18 @@ const useLayoutedElements = () => {
         layoutOptions: layoutOptions,
         children: getNodes().map((node) => ({
           ...node,
-          width: node.measured?.width,
-          height: node.measured?.height,
+          width: 100,
+          height: 30,
         })),
         edges: getEdges(),
       };
+      console.log("Graph")
+      console.log(graph)
    
       elk.layout(graph).then(({ children }) => {
         // By mutating the children in-place we saves ourselves from creating a
         // needless copy of the nodes array.
-        console.log(children)
+        console.log("Iterating over children")
         children?.forEach((node :any) => {
           console.log("Ran")
           node.position = { x: node.x, y: node.y } as XYPosition;
@@ -67,10 +70,11 @@ const AnalysisPageFlowGraph = (props :AnalysisPageFlowGraphProps) => {
 
     const [isUpdating, setIsUpdating] = useState<Boolean>(false)
     const [data, setData] = useState<any>()
-    const [canUpdateGraph, setCanUpdateGraph] = useState<Boolean>(false)
+    const [updateRequired, setUpdateReqired] = useState<Boolean>(true)
 
     useEffect(() => {
-      if(!isUpdating && !canUpdateGraph) {
+      console.log("Is updating")
+      if(props.edges?.length == 0) {
         fetch("http://localhost:3000/api/nodes", {headers:{'Access-Control-Allow-Origin': '*'}}).then((result :Response) => {
                 result.json().then((response) => {
                     // Create a temp holder to limit number of updates required
@@ -79,41 +83,33 @@ const AnalysisPageFlowGraph = (props :AnalysisPageFlowGraphProps) => {
 
                     for(let index = 0; index < response.length; index++){
                         holder.push({ id: response[index][0], position: { x: 0 + index * 100, y: 100 * index }, data: { label: response[index][0] }})
-                        tempEdges.push({ id: response[index][0], source: response[index][0], target: response[3][0], type:"custom-edge"})
+                        tempEdges.push({ id: response[index][0], source: response[index][0], target: response[3][0], type:"custom-edge", animated:true})
                     }
                     console.log("nodes updated")
                     console.log("Edges")
-                    if(props.nodes == holder){
-                      return
-                    }
-                    props.setNodes(holder)
-                    console.table(tempEdges)
-                    console.table(holder)
-                    setData(tempEdges)
 
-                })
-                setIsUpdating(true)    
+                    props.setEdges(tempEdges)
+                    props.setNodes(holder)
+
+                    setIsUpdating(false)
+                })   
         })
         }
-        if(props.nodes != undefined) {
-          if(props.nodes?.length > 1 && isUpdating && !canUpdateGraph) {
-            props.setEdges(data)
-            setCanUpdateGraph(true)
-            setIsUpdating(false)
-          }
+        else {
+          console.log("waiting")
+          wait(2000)
+          console.log("waited")
         }
-        if(canUpdateGraph) {
-          console.log("Updated")
-          getLayoutedElements({'elk.algorithm': 'org.eclipse.elk.radial'})
-          setCanUpdateGraph(true)
-        }
-    }, [canUpdateGraph, isUpdating]) 
+    }, [props.edges]) 
 
     return(
 
         <article>
+          <p onClick={() => {getLayoutedElements({})}}>Update graph</p>
             <div style={{ width: '100vw', height: '100vh' }}>
-                <ReactFlow nodes={props.nodes} edges={props.edges} edgeTypes={edgeTypes} onNodesChange={props.onNodesChange} onEdgesChange={props.onEdgesChange} />
+                <ReactFlow nodes={props.nodes} edges={props.edges} edgeTypes={edgeTypes} onNodesChange={props.onNodesChange} onEdgesChange={props.onEdgesChange}>
+                  <MiniMap />  
+                </ReactFlow>
             </div>
         </article>
     )
